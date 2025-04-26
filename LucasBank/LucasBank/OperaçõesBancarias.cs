e977d6a1ee01815f2e421f069b86bf5024e17c72
luckfer7 <lucasferreira059@gmail.com>
2025-04-26 20:25:31 -0300

    funções básicas

---
+++ LucasBank/LucasBank/OperaçõesBancarias.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasBank
+{
+    class OperaçõesBancarias
+    {
+        decimal saldo = 0;
+
+        public void ConsultarSaldo()
+        {
+            Console.WriteLine($"Seu saldo atual: {saldo}");
+        }
+
+        public void DepositarDinheiro()
+        {
+            Console.WriteLine("Digite o valor que quer depositar: ");
+            if (decimal.TryParse(Console.ReadLine(), out decimal valorDepositado) && valorDepositado > 0)
+            {
+                saldo = saldo + valorDepositado;
+                Console.WriteLine("Valor foi depositado com sucesso!");
+                Console.WriteLine($"Seu saldo atual: {saldo}");
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido");
+            }
+        }
+
+        public void SacarDinheiro()
+        {
+            Console.WriteLine("Informe o valor que deseja sacar: ");
+            if (decimal.TryParse(Console.ReadLine(), out decimal valorASacar) && valorASacar > 0)
+            {
+                if (valorASacar <= saldo)
+                {
+                    saldo = saldo - valorASacar;
+                    Console.WriteLine("Espere um pouco estamos processando o seu pedido...");
+                    Console.WriteLine("Valor sacado com sucesso!");
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido ou é maior que o saldo disponível.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido!");
+            }
+        }
+    }
+}
---
+++ LucasBank/LucasBank/TerminalDeOperações.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasBank
+{
+    public class TerminalDeOperações
+    {
+        OperaçõesBancarias banco = new OperaçõesBancarias();
+
+        public void IniciarPrograma()
+        {
+            
+
+            while (true)
+            {
+                Menu();
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        banco.ConsultarSaldo();
+                    break;
+
+                    case "2":
+                        banco.DepositarDinheiro();
+                    break;
+
+                    case "3":
+                        banco.SacarDinheiro();
+                    break;
+                }
+
+            }
+        }
+
+        public void Menu ()
+        {
+            Console.WriteLine("\n===bank===\n");
+
+            Console.WriteLine("Bem vindo, Lucas, a sua conta corrente: ");
+            Console.WriteLine("Escolha a opçao correspondente");
+            Console.WriteLine("1. Consultar saldo");
+            Console.WriteLine("2. Depositar dinheiro");
+            Console.WriteLine("3. Sacar dinheiro");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+    }
+}
